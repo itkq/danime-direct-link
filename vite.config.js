33c02fca02359da716b16b3c6fe5114fbba3e603
Takuya Kosugiyama <re@itkq.jp>
2023-01-02 22:23:15 +0900

    Use background worker with executeScript

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,13 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: 'danime direct link',
   version: '0.0.1',
-  content_scripts: [
-    {
-      matches: ["https://animestore.docomo.ne.jp/animestore/ci_pc?workId=*&partId=*"],
-      js: ["src/link.ts"],
-    }
-  ]
+  permissions: ["tabs", "scripting"],
+  host_permissions: [
+    "https://animestore.docomo.ne.jp/animestore/ci_pc?workId=*&partId=*"
+  ],
+  background: {
+    service_worker: "src/background.ts",
+  },
 })
 
 export default defineConfig({
